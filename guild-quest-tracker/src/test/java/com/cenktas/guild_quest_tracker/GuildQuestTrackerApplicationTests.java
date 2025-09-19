46f49151c0fded176dcf6a3c9d480ee8fe2a6c25
Cenk Berkay Taş <cenkbtas@gmail.com>
2025-09-19 20:18:44 +0300

    init

---
+++ guild-quest-tracker/src/test/java/com/cenktas/guild_quest_tracker/GuildQuestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenktas.guild_quest_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildQuestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
